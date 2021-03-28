5d5052685cda9120baddd858d2e32da00f8dd44a
belanov-ak <a.k.belanov@gmail.com>
2021-03-28 13:31:30 +0300

    project flow reworked

--- gulpfile.js
+++ gulpfile.js
@@ -13,13 +13,15 @@ const rename = require('gulp-rename')
 const uglify = require('gulp-uglify-es').default
 const babel = require('gulp-babel')
 const imagemin = require('gulp-imagemin')
-const webp = require('gulp-webp')
-const webpHTML = require('gulp-webp-html')
-const webpCSS = require('gulp-webpcss')
 const svgSprite = require('gulp-svg-sprite')
 const ttf2woff = require('gulp-ttf2woff')
 const ttf2woff2 = require('gulp-ttf2woff2')
 const fs = require('fs')
+const plumber = require('gulp-plumber')
+const sourcemaps = require('gulp-sourcemaps')
+// const webp = require('gulp-webp')
+// const webpHTML = require('gulp-webp-html')
+// const webpCSS = require('gulp-webpcss')
 
 const path = {
     build: {
@@ -48,34 +50,30 @@ const path = {
     clean: './' + project_folder + '/'
 }
 
-function browserSync(params) {
-    browsersync.init({
-       server: {
-           baseDir: './'+project_folder+'/'
-       },
-       port: 3000,
-       notify: false
-    })
+
+function clean(params) {
+    return del(path.clean)
 }
 
 function html() {
     return src(path.src.html)
+        .pipe(plumber())
         .pipe(fileinclude())
-        .pipe(webpHTML())
+        // .pipe(webpHTML())
         .pipe(dest(path.build.html))
         .pipe(browsersync.stream())
 }
 
 function images() {
     return src(path.src.img)
-        .pipe(
-            webp({
-                quality: 700
-            })
-        )
+        // .pipe(
+        //     webp({
+        //         quality: 10
+        //     })
+        // )
         .pipe(dest(path.build.img))
         .pipe(src(path.src.img))
-        .pippe(
+        .pipe(
             imagemin({
                 progressive: true,
                 svgoPlugins: [{ removeViewBox: false}],
@@ -89,6 +87,8 @@ function images() {
 
 function js() {
     return src(path.src.js)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(fileinclude())
         .pipe(babel({
             presets: ['@babel/env']
@@ -96,12 +96,9 @@ function js() {
         .pipe(dest(path.build.js))
         .pipe(uglify())
         .pipe(rename({extname: '.min.js'}))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest(path.build.js))
         .pipe(browsersync.stream())
-}
-
-function clean(params) {
-    return del(path.clean)
 }
 
 function fonts(params) {
@@ -115,6 +112,8 @@ function fonts(params) {
 
 function css() {
     return src(path.src.css)
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(
             scss({
                 outputStyle: 'expanded'
@@ -125,10 +124,11 @@ function css() {
                 cascade: true
         }))
         .pipe(gcmq())
-        .pipe(webpCSS())
+        // .pipe(webpCSS())
         .pipe(dest(path.build.css))
         .pipe(cleanCSS())
         .pipe(rename({extname: '.min.css'}))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest(path.build.css))
         .pipe(browsersync.stream())
         
@@ -146,28 +146,28 @@ gulp.task('svgSprite', function () {
     .pipe(dest(path.build.img))
 })
 
-function fontsStyle(params) {
+// function fontsStyle(params) {
 
-    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
-    if (file_content == '') {
-        fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
-        return fs.readdir(path.build.fonts, function (err, items) {
-            if (items) {
-                let c_fontname;
-                for (var i = 0; i < items.length; i++) {
-                    let fontname = items[i].split('.');
-                    fontname = fontname[0];
-                    if (c_fontname != fontname) {
-                        fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
-                    }
-                    c_fontname = fontname;
-                }
-            }
-        })
-    }
-}
+//     let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+//     if (file_content == '') {
+//         fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+//         return fs.readdir(path.build.fonts, function (err, items) {
+//             if (items) {
+//                 let c_fontname;
+//                 for (var i = 0; i < items.length; i++) {
+//                     let fontname = items[i].split('.');
+//                     fontname = fontname[0];
+//                     if (c_fontname != fontname) {
+//                         fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+//                     }
+//                     c_fontname = fontname;
+//                 }
+//             }
+//         })
+//     }
+// }
     
-    function cb() { }
+// function cb() { }
 
 function watchFiles(params) {
     gulp.watch([path.watch.html], html)
@@ -176,10 +176,21 @@ function watchFiles(params) {
     gulp.watch([path.watch.img], images)
 }
 
-const build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle)
+function browserSync(params) {
+    browsersync.init({
+       server: {
+           baseDir: './' + project_folder + '/'
+       },
+       port: 3000,
+       notify: false
+    })
+}
+
+const build = gulp.series(gulp.parallel(js, css, html, images, fonts))
+// const build = gulp.series(gulp.parallel(js, css, html, images, fonts), fontsStyle)
 const watch = gulp.parallel(build, watchFiles, browserSync)
 
-exports.fontsStyle = fontsStyle
+// exports.fontsStyle = fontsStyle
 exports.fonts = fonts
 exports.images = images
 exports.js = js
